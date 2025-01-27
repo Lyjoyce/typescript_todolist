ecdd941b866d699a875c063fb86653776b482c37
lyjoice <lahely.na@gmail.com>
2025-01-27 21:14:31 +0100

    script ""

--- src/app.ts
+++ src/app.ts
@@ -6,3 +6,11 @@ interface Task {
 
 let tasks: Task[] = []
 let taskId = 0
+
+const taskInput= document.getElementById("taskInput") as HTMLInputElement
+const addTaskButton= document.getElementById("addTaskButton") as HTMLButtonElement
+const taskList= document.getElementById("taskList") as HTMLUListElement
+
+addTaskButton.addEventListener("click", () => 
+{console.log ("coucou")
+ })

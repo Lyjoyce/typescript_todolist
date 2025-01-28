f4f278ed8977690fb3c14a8af821ed77d10054c5
lyjoice <lahely.na@gmail.com>
2025-01-28 21:01:02 +0100

    modif script

---
+++ dist/app.js
@@ -0,0 +1,9 @@
+"use strict";
+let tasks = [];
+let taskId = 0;
+const taskInput = document.getElementById("taskInput");
+const addTaskButton = document.getElementById("addTaskButton");
+const taskList = document.getElementById("taskList");
+addTaskButton.addEventListener("click", () => {
+    console.log("coucou");
+});
